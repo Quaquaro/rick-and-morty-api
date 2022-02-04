12c390b57cdcd5c75a434e42b57b51d63260a9ab
Andrea Hohnsen <a.hohnsen@mail.de>
2022-02-04 20:56:57 +0100

    Delete button component

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-import Button from './components/Button.js';
 import Search from './components/Search.js';
-
-const buttons = document.querySelectorAll('[data-js=Button]');
-
-buttons.forEach(Button);
 
 Search();

83c20d502503a469035fa84a2f3df1bd7bd37da4
Christoph Timm <quaquaro@gmail.com>
2022-02-04 20:40:15 +0100

    Add some styling fetch all data over all pages

--- components/Search.js
+++ components/Search.js
@@ -1,57 +1,74 @@
-export default async function Search() {
-  const response = await fetch(
-    `https://rickandmortyapi.com/api/character/`
-  ).catch(error => console.error(error));
-  const data = await response.json();
+export default function Search() {
+  getAllData();
 
-  const charactersData = data.results;
-  let filteredCharacters = charactersData;
-  const searchForm = getQuerySelector('search-form');
-  const searchResults = getQuerySelector('search-results');
-  const searchInput = getQuerySelector('search-input');
-  const filterForm = getQuerySelector('filter-form');
-  let currentFilter = 'all';
+  function getAllData() {
+    const allPages = [];
 
-  renderCharacters();
+    for (let i = 1; i < 10; i++) {
+      allPages.push(`https://rickandmortyapi.com/api/character/?page=${i}`);
+    }
 
-  searchForm.addEventListener('submit', event => event.preventDefault());
-  searchInput.addEventListener('input', getSearchResults);
-  filterForm.addEventListener('change', () => {
-    currentFilter = filterForm.elements['liveStatus'].value;
-    renderCharacters();
-  });
+    let allData = [];
 
-  function getSearchResults() {
-    const userInput = searchInput.value.trim().toLowerCase();
-    filteredCharacters = charactersData.filter(character =>
-      character.name.toLowerCase().includes(userInput)
-    );
-    renderCharacters();
-  }
+    allPages.forEach(page => fetchAllData(page));
 
-  function renderCharacters() {
-    searchResults.innerHTML = '';
-    filteredCharacters
-      .filter(
-        character =>
-          character.status.toLowerCase() === currentFilter.toLowerCase() ||
-          currentFilter === 'all'
-      )
-      .forEach(character => {
-        const characterElement = document.createElement('li');
-        characterElement.className = 'Search__character';
-        characterElement.innerHTML = `
-        <h2>${character.name}</h2>
-        <img class="Search__character-image" src="${character.image}" alt="${character.name}" width="300" height="300">
-        <p>Status: ${character.status}</p>
-        <p>Species: ${character.species}</p>
-        <p>Origin: ${character.origin.name}</p>`;
-        searchResults.append(characterElement);
+    async function fetchAllData(page) {
+      const response = await fetch(page).catch(error => console.error(error));
+
+      const currentData = await response.json();
+      allData.push(...currentData.results);
+      const charactersData = allData;
+
+      let filteredCharacters = charactersData;
+      const searchForm = getQuerySelector('search-form');
+      const searchResults = getQuerySelector('search-results');
+      const searchInput = getQuerySelector('search-input');
+      const filterForm = getQuerySelector('filter-form');
+      let currentFilter = 'all';
+
+      renderCharacters();
+
+      searchForm.addEventListener('submit', event => event.preventDefault());
+      searchInput.addEventListener('input', getSearchResults);
+      filterForm.addEventListener('change', () => {
+        currentFilter = filterForm.elements['liveStatus'].value;
+
+        renderCharacters();
       });
-  }
 
-  function getQuerySelector(datajs) {
-    return document.querySelector(`[data-js="${datajs}"]`);
+      function getSearchResults() {
+        const userInput = searchInput.value.trim().toLowerCase();
+        filteredCharacters = charactersData.filter(character =>
+          character.name.toLowerCase().includes(userInput)
+        );
+
+        renderCharacters();
+      }
+
+      function renderCharacters() {
+        searchResults.innerHTML = '';
+        filteredCharacters
+          .filter(
+            character =>
+              character.status.toLowerCase() === currentFilter.toLowerCase() ||
+              currentFilter === 'all'
+          )
+          .forEach(character => {
+            const characterElement = document.createElement('li');
+            characterElement.className = 'Search__character';
+            characterElement.innerHTML = `
+            <h2>${character.name}</h2>
+            <img class="Search__character-image" src="${character.image}" alt="${character.name}" width="300" height="300">
+            <p>Status: ${character.status}</p>
+            <p>Species: ${character.species}</p>
+            <p>Origin: ${character.origin.name}</p>`;
+            searchResults.append(characterElement);
+          });
+      }
+
+      function getQuerySelector(datajs) {
+        return document.querySelector(`[data-js="${datajs}"]`);
+      }
+    }
   }
 }
-// https://stackoverflow.com/questions/68366280/filter-by-includes-and-then-sort-by-startswith
